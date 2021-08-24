e6e2287712401d2d6ebcce81ba97a5bebc6ef121
ethanial1 <camiloeiz00@gmail.com>
2021-08-24 14:15:06 -0500

    Definimos algunos estados

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 function App() {
   // Definimos algunos estados
   const [username, setuserName] = useState('ethan');
+  const [taskItems, settaskItems] = useState([
+    {
+      name: "Task one",
+      done: false
+    }
+  ]);
   
   return (
     <div className="App">
