27f37e2628c41c40b8392417def81fa191fc6b6d
ethanial1 <camiloeiz00@gmail.com>
2021-08-24 14:57:36 -0500

    Mostramos las tareas en una tabla, manejador para el checkBox

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import { useState } from 'react';
+import { TaskRow } from './components/TaskRow';
 import './App.css';
 
 function App() {
   // Definimos algunos estados
   const [username, setuserName] = useState('ethan');
   const [taskItems, settaskItems] = useState([
-    {
-      name: "Task one",
-      done: false
-    }
+    { name: "Task one", done: false },
+    { name: "Task two", done: true },
+    { name: "Task three", done: false },
+    { name: "Task Four", done: false },
   ]);
-  
+
+  // Cambiamos el estado de DONE, recorriendo cada tarea                            se hace una copia (...)
+  const toggleTask = task => settaskItems(taskItems.map(t => (t.name === task.name ? {...t, done: !t.done} : t)));
+
+  // Crea multipler tr que luego son pintados en la tabla
+  const taskTableRows = () => taskItems.map( task => (
+      <TaskRow task={task} key={task.name} toggleTask={toggleTask}/>
+    ));
+   
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>
+          {taskTableRows()}
+        </tbody>
+      </table>
+      
     </div>
   );
 }
--- src/components/TaskRow.js
+++ src/components/TaskRow.js
@@ -1,1 +1,12 @@
 // Guarda las filas de las tablas que vamos a crear para mostrar las tareas (datos)
+
+export const TaskRow = props => (
+    <tr key={props.task.name}>
+        <td>
+            {props.task.name}
+        </td>
+        <td>
+            <input type="checkbox" id="" checked = {props.task.done} onChange={props.toggleTask}/>
+        </td>
+    </tr>
+);
